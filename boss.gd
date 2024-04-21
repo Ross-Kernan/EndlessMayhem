b5d7aec70608afe2b240ed2649d3757d00066960
Ross-Kernan <falliewagon@gmail.com>
2024-04-21 23:19:51 +0100

    added new sprites

--- boss.gd
+++ boss.gd
@@ -1,19 +1,21 @@
 extends CharacterBody2D
 
 var health = 300
+var BOSS_SPEED = 100
 
 @onready var player = get_node("/root/Game/player")
 
 
 
 func _physics_process(_delta):
+	%BigRobot.set_texture(load("res://PixelPlat/Tiles/Characters/tile_0008.png"))
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 100.0
+	velocity = direction * 100.0 #BOSS_SPEED
 	move_and_slide()
 	
-	#if health <= 200:
-		#%BigRobot.texture = load("res://PixelPlat/Tiles/Characters/tile_0024.png")
-
+	if health <= 200:
+		BOSS_SPEED += 10
+		%BigRobot.set_texture(load("res://TestAssets/angry_boss.png"))
 
 func take_damage():
 	health -= 1
